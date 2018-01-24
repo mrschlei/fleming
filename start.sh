cb54a3bd040329406885ae21e8678afc87ecd5ca
Ben Fairfield <benfa@umich.edu>
2018-01-24 14:03:22 -0500

    Added Dockerfile and start.sh

---
+++ start.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+# Redirect logs to stdout and stderr for docker reasons.
+ln -sf /dev/stdout /var/log/apache2/access_log
+ln -sf /dev/stderr /var/log/apache2/error_log
+
+# Apache and virtual host secrets
+ln -sf /secrets/apache2/apache2.conf /etc/apache2/apache2.conf
+ln -sf /secrets/apache2/ports.conf /etc/apache2/ports.conf
+ln -sf /secrets/apache2/default-ssl.conf /etc/apache2/sites-available/default-ssl.conf
+ln -sf /secrets/apache2/cosign.conf /etc/apache2/mods-available/cosign.conf
+
+# app secrets
+ln -sf /secrets/app/settings.php /var/www/html/sites/default/settings.php
+
+# SSL secrets
+ln -sf /secrets/ssl/USERTrustRSACertificationAuthority.pem /etc/ssl/certs/USERTrustRSACertificationAuthority.pem
+ln -sf /secrets/ssl/AddTrustExternalCARoot.pem /etc/ssl/certs/AddTrustExternalCARoot.pem
+ln -sf /secrets/ssl/sha384-Intermediate-cert.pem /etc/ssl/certs/sha384-Intermediate-cert.pem
+ln -sf /secrets/ssl/its-backstage.openshift.dsc.umich.edu.cert /etc/ssl/certs/its-backstage.openshift.dsc.umich.edu.cert
+ln -sf /secrets/ssl/its-backstage.openshift.dsc.umich.edu.key /etc/ssl/private/its-backstage.openshift.dsc.umich.edu.key
+
+## Rehash command needs to be run before starting apache.
+c_rehash /etc/ssl/certs
+
+a2enmod ssl
+a2enmod include
+a2ensite default-ssl 
+
+## set SGID for www-data 
+chown -R www-data.www-data /var/www/html /var/cosign
+chmod -R 2775 /var/www/html /var/cosign
+
+/usr/local/bin/apache2-foreground
